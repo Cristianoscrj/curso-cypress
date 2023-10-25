135f5094820e565e169ae049202cfe0d7215ed8c
Cristianoscrj <cristiano_clock@hotmail.com>
2023-10-25 14:29:32 -0300

    testando

--- cypress/e2e/testando.cy.js
+++ cypress/e2e/testando.cy.js
@@ -1,11 +1,11 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:4200/#/home')
-    cy.contains ('a', 'Register now').click(;)
-    cy.get ('[formcontrolname="email"]').type('cristiano@email.com.br')
-    cy.get ('[formcontrolname="FullName"]').type('Cristiano Costa')
-    cy.get ('[formcontrolname="userName"]').type('Cristiano Costa')
-    cy.get ('[formcontrolname="password"]').type('cris123')
-    cy.contains ('button', 'Register').click(;)
+    cy.contains ('a', 'Register now').click();
+    cy.get('[formcontrolname="email"]').type('cristiano@email.com.br')
+    cy.get('[formcontrolname="fullName"]').type('Cristiano Costa')
+    cy.get('[formcontrolname="userName"]').type('cristianosc')
+    cy.get('[formcontrolname="password"]').type('cris123')
+    cy.contains ('button', 'Register').click();
   })
 })
