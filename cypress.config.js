64ce0e9c66fcc4d08e4f6959619425eade0d4f92
Cristianoscrj <cristiano_clock@hotmail.com>
2023-10-30 14:50:50 -0300

    teste

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+      setupNodeEvents(on, config) {
+          { "reporter"; "mochawesome",
+              "reporterOptions"; 
+                  { "reportDir"; "cypress/report/mochawesome-report",
+                  "overwrite"; true,
+                  "html"; true,
+                  "json"; false,
+                  "timestamp"; "mmddyyyy_HHMMss" }}
+      },
   },
-};
+});
