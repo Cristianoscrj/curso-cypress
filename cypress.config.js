67ae8da0f2a17164eded65c47c0dca728cfbc8b4
Cristianoscrj <cristiano_clock@hotmail.com>
2023-10-30 14:57:45 -0300

    teste

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const {defineConfig} = require ("cypress";)
 module.exports = defineConfig({
   e2e: {
       setupNodeEvents(on, config) {
+        module.exports = defineConfig({
+          e2e: {
+            setupNodeEvents(on, config) { 
           { "reporter"; "mochawesome",
               "reporterOptions"; 
                   { "reportDir"; "cypress/report/mochawesome-report",
