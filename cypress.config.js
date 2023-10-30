8e67820fb0788365f54b5dae4ee0b37a8c385266
Cristianoscrj <cristiano_clock@hotmail.com>
2023-10-30 15:11:50 -0300

    teste

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,7 @@
-const {defineConfig} = require("cypress");
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
-      setupNodeEvents(on, config) {
-        module.exports = defineConfig({
-          e2e: {
-            setupNodeEvents(on, config) { 
-          { "reporter"; "mochawesome",
-              "reporterOptions"; 
-                  { "reportDir"; "cypress/report/mochawesome-report",
-                  "overwrite"; true,
-                  "html"; true,
-                  "json"; false,
-                  "timestamp"; "mmddyyyy_HHMMss" }}
-      },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
-});
+};
