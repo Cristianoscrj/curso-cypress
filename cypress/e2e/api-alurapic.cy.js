a79a4de35b6e0a13824f9e1dd159c5f9b26e1e33
Cristianoscrj <cristiano_clock@hotmail.com>
2023-11-06 16:31:50 -0300

    ok

--- cypress/e2e/api-alurapic.cy.js
+++ cypress/e2e/api-alurapic.cy.js
@@ -1,17 +1,20 @@
-describe('Api Alurapic', () => {
+describe('Buscar fotos e dados', () => {
 
     // código comentado omitido…
     
-        it('Fotos do usuário', () => {
+        it('buscar fotos do usuário', () => {
+          
+          const tempoEsperado = Math.random() * 50;
+    
             cy.request({
                 method: 'GET' ,
-                url: 'http://localhost:3000/cristianocosta/photos'
+                url: 'http://localhost:3000/carowl/photos'
             }).then((res) => {
                 expect(res.status).to.be.equal(200)
                 expect(res.body).is.not.empty
                 expect(res.body[0]).to.have.property('description')
-                expect(res.body[0].description).to.be.equal('pedrinho')
-                
+                expect(res.body[0].description).to.be.equal('porquinho')
+                expect(res.duration).to.be.lte(tempoEsperado)
             })
         })
     })
