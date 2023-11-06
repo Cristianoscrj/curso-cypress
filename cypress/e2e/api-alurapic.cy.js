db5c07bc08bad4d08accf4910b3efaee327b62a1
Cristianoscrj <cristiano_clock@hotmail.com>
2023-11-06 16:23:19 -0300

    ok

--- cypress/e2e/api-alurapic.cy.js
+++ cypress/e2e/api-alurapic.cy.js
@@ -1,14 +1,18 @@
 describe('Api Alurapic', () => {
-    it('Dados da API', () => {
+
+    // código comentado omitido…
+    
+        it('Fotos do usuário', () => {
             cy.request({
-                method: 'POST',
-                url: 'http://localhost:3000/user/login',
-                body: Cypress.env()
+                method: 'GET' ,
+                url: 'http://localhost:3000/carowl/photos'
             }).then((res) => {
                 expect(res.status).to.be.equal(200)
                 expect(res.body).is.not.empty
-                expect(res.body).to.have.property('id')
-                expect(res.body.id).to.be.equal(4)
+                expect(res.body[0]).to.have.property('description')
+                expect(res.body[0].description).to.be.equal('porquinho')
+                
             })
+        })
     })
-})
+    
