9c74e0228980b75fccfcd3ab853bf651b8c815cb
Cristianoscrj <cristiano_clock@hotmail.com>
2023-11-06 09:11:33 -0300

    teste

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -1,8 +1,18 @@
 describe('Página de login', () => {
-    it('Verificar mensagem de campos obrigatórios', () => {
-      cy.visit('http://localhost:4200/#/home');
+  beforeEach(() => {
+      cy.visit('http://localhost:4200');
+      cy.intercept('POST', 'http://localhost:3000/user/login', {
+              statusCode: 400
+              }).as('stubPost')
+  })
+      
+it('Verifica mensagens de campos obrigatórios na página de login', () => {
       cy.contains('User name is required!').should('be.visible');
       cy.contains('Password is required!').should('be.visible');
-    });
-  });
-  
+  })
+      
+      it('Deve falhar mesmo que os campos sejam preenchidos corretamente', () => {
+      cy.login('carowl', '12345678')
+              cy.wait('@stubPost')
+  })
+})
