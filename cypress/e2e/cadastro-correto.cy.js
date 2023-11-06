59ce2337310e46e1b2d8419759452b024f41a322
Cristianoscrj <cristiano_clock@hotmail.com>
2023-11-06 16:47:54 -0300

    ok

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -1,11 +1,18 @@
+import Cadastro from '../support/pages/cadastro/pagina-cadastro';
+
 describe('Página de cadastro', () => {
-  it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
-    cy.visit('http://localhost:4200/#/home')
-    cy.contains ('a', 'Register now').click();
-    cy.get('[data-test="email"]').type('cristiano@email.com.br')
-    cy.get('[data-test="fullName"]').type('Cristiano Costa')
-    cy.get('[data-test="registerUserName"]').type('cristianocosta')
-    cy.get('[data-test="registerPassword"]').type('cristiano123')
-    cy.contains ('button', 'Register').click();
+
+    beforeEach(() => {
+        cy.visit('http://localhost:4200')
+    }) 
+            
+        it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+
+            // código comentado omitido…
+
+            Cadastro.acessarPaginaDeCadastro();
+            Cadastro.preencherFormulario();
+            Cadastro.submeterCadastro();
+        
+        })
   })
-})
